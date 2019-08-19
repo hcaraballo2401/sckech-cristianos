4e8b43bdb3fc9435b89027d0c02cbbcf69a558fa
hcaraballo2401 <hcaraballo2401@gmail.com>
2019-08-19 11:41:07 -0400

    sckech de obra

---
+++ prot.js
@@ -0,0 +1,139 @@
+function play(){
+    var inicio=document.getElementById("audio");
+    inicio.play();
+}
+
+function play1(){
+    var creacion=document.getElementById("audio1");
+    creacion.play();
+}
+
+function play2(){
+    var creacion=document.getElementById("audio2");
+    creacion.play();
+}
+
+function play3(){
+    var creacion=document.getElementById("audio3");
+    creacion.play();
+}
+
+function play4(){
+    var creacion=document.getElementById("audio4");
+    creacion.play();
+}
+
+function play5(){
+    var creacion=document.getElementById("audio5");
+    creacion.play();
+}
+
+function play6(){
+    var creacion=document.getElementById("audio6");
+    creacion.play();
+}
+
+function play7(){
+    var creacion=document.getElementById("audio7");
+    creacion.play();
+}
+
+function play8(){
+    var creacion=document.getElementById("audio8");
+    creacion.play();
+}
+
+function play9(){
+    var creacion=document.getElementById("audio9");
+    creacion.play();
+}
+
+function play10(){
+    var creacion=document.getElementById("audio10");
+    creacion.play();
+}
+
+function play11(){
+    var creacion=document.getElementById("audio11");
+    creacion.play();
+}
+
+function play12(){
+    var creacion=document.getElementById("audio12");
+    creacion.play();
+}
+
+function play13(){
+    var creacion=document.getElementById("audio13");
+    creacion.play();
+}
+
+function play14(){
+    var creacion=document.getElementById("audio14");
+    creacion.play();
+}
+
+function play15(){
+    var creacion=document.getElementById("audio15");
+    creacion.play();
+}
+
+function play16(){
+    var creacion=document.getElementById("audio16");
+    creacion.play();
+}
+
+function play17(){
+    var creacion=document.getElementById("audio17");
+    creacion.play();
+}
+
+function play18(){
+    var creacion=document.getElementById("audio18");
+    creacion.play();
+}
+
+function play19(){
+    var creacion=document.getElementById("audio19");
+    creacion.play();
+}
+
+function play20(){
+    var creacion=document.getElementById("audio20");
+    creacion.play();
+}
+
+function play21(){
+    var creacion=document.getElementById("audio21");
+    creacion.play();
+}
+
+function play22(){
+    var creacion=document.getElementById("audio22");
+    creacion.play();
+}
+
+function play23(){
+    var creacion=document.getElementById("audio23");
+    creacion.play();
+}
+
+function play24(){
+    var creacion=document.getElementById("audio24");
+    creacion.play();
+}
+
+function play25(){
+    var creacion=document.getElementById("audio25");
+    creacion.play();
+}
+
+function play26(){
+    var creacion=document.getElementById("audio26");
+    creacion.play();
+}
+
+function play27(){
+    var creacion=document.getElementById("audio27");
+    creacion.play();
+}
